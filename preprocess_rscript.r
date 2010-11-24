2e813d70ac624e2707059be580d45a136e3853ed
Mike Dewar <md2954@columbia.edu>
2010-11-24 11:46:45 -0500

    commented out everything but the preprocessing on the cluster.

--- preprocess_rscript.r
+++ preprocess_rscript.r
@@ -14,5 +14,5 @@ out_file = "/ifs/home/c2b2/cw_lab/md2954/Data/immgen/userData/GSE15907/immgen.da
 
 
 immgen.preprocess(root_folder,summary_file,GEOid=GEOid)
-immgen.form_symbol_file(symbol_file)
-immgen.form_expression_set(summary_file,symbol_file,out_file)
+#immgen.form_symbol_file(symbol_file)
+#immgen.form_expression_set(summary_file,symbol_file,out_file)
