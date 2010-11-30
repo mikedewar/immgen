086bc520b2d0d8937c318a6f57eef348ee516969
Mike Dewar %Wiggins <md2954@b1a.c3.titan>
2010-11-30 19:01:10 +0000

    still doesn't work on the cluster nodes. Can't figure out why

--- preprocess_rscript.r
+++ preprocess_rscript.r
@@ -7,7 +7,6 @@ logger <- create.logger()
 logfile(logger) <- file.path('/ifs/scratch/c2b2/cw_lab/md2954/Logs/immgen_preprocess.log')
 level(logger) <- log4r:::DEBUG
 
-
 source('preprocess.r')
 
 GEOid = "GSE15907"
@@ -16,10 +15,6 @@ root_folder = "/ifs/scratch/c2b2/cw_lab/md2954/immgen"
 summary_file = paste(root_folder,"userData",GEOid,"gene_summary.data",sep="/")
 symbol_file = paste(root_folder,"userData",GEOid,"probes2symbols.data",sep="/")
 out_file = paste(root_folder,"userData",GEOid,"immgen.data",sep="/")
-
-# let's just check we can access the root folder
-out = file("/ifs/scratch/c2b2/cw_lab/md2954/immgen/rawData/GSE15907/MoGene-1_0-st-v1/GSM538309_EA07068_87578_MoGene_NK.H+MCMV1#1.CEL","wb")
-debug(logger, print(out))
 
 immgen.preprocess(root_folder,summary_file,GEOid=GEOid)
 immgen.form_symbol_file(symbol_file)
