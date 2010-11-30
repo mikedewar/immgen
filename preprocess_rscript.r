a33c43183bd37b9b30a7cfe9e745bd812da544f4
Mike Dewar <md2954@columbia.edu>
2010-11-30 13:14:08 -0500

    added a check to see if we have write access to the root folder

--- preprocess_rscript.r
+++ preprocess_rscript.r
@@ -1,16 +1,24 @@
 # this script specifies the necessary folders for the immgen preprocessing script.
 # this assumes you've already run preprocess_setup.py which sets up the folder 
 # structure for aroma
+logger <- create.logger()
+logfile(logger) <- file.path('/ifs/scratch/c2b2/cw_lab/md2954/Logs/immgen_preprocess.log')
+level(logger) <- log4r:::DEBUG
+
 
 source('preprocess.r')
 
-root_folder = "/ifs/scratch/c2b2/cw_lab/md2954/immgen"
 GEOid = "GSE15907"
-summary_file = "/ifs/scratch/c2b2/cw_lab/md2954/immgen/userData/GSE15907/gene_summary.data"
-symbol_file = "/ifs/scratch/c2b2/cw_lab/md2954/immgen/userData/GSE15907/probes2symbols.data"
-out_file = "/ifs/scratch/c2b2/cw_lab/md2954/immgen/userData/GSE15907/immgen.data"
 
+root_folder = "/ifs/scratch/c2b2/cw_lab/md2954/immgen"
+summary_file = paste(root_folder,"userData",GEOid,"gene_summary.data",sep="/")
+symbol_file = paste(root_folder,"userData",GEOid,"probes2symbols.data",sep="/")
+out_file = paste(root_folder,"userData",GEOid,"immgen.data",sep="/")
+
+# let's just check we can access the root folder
+out = file(root_folder,"wb")
+log.debug(str(out))
 
 immgen.preprocess(root_folder,summary_file,GEOid=GEOid)
-#immgen.form_symbol_file(symbol_file)
-#immgen.form_expression_set(summary_file,symbol_file,out_file)
+immgen.form_symbol_file(symbol_file)
+immgen.form_expression_set(summary_file,symbol_file,out_file)
