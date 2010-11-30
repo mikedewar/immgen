ca0912041de533be21f5c4d58dba9cec2aea0f6a
Mike Dewar <md2954@columbia.edu>
2010-11-30 10:40:54 -0500

    updated path to use c2b2's scratch drive (boring now)

--- runscript.sh
+++ runscript.sh
@@ -4,6 +4,10 @@
 #$ -m beas
 #$ -l mem=15G,time=24:00:00
 #$ -cwd
-#$ -o /ifs/home/c2b2/cw_lab/md2954/Data/immgen.out
-#$ -e /ifs/home/c2b2/cw_lab/md2954/Data/immgen.err
+#$ -o /ifs/scratch/c2b2/cw_lab/md2954/Logs/immgen.out
+#$ -e /ifs/scratch/c2b2/cw_lab/md2954/Logs/immgen.err
+
+# one needs to run the following before letting this script onto the cluster
+#python preprocess_setup.py /ifs/scratch/c2b2/cw_lab/md2954/immgen
+
 /nfs/apps/R/2.11.1/bin/Rscript /ifs/home/c2b2/cw_lab/md2954/Projects/immgen/preprocess_rscript.r
